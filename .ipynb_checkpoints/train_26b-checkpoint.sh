54f8536baae9d01f4c2b2b5352d9452787a0b3bf
sheldonldev <xiadanli0320@gmail.com>
2024-08-24 18:12:11 +0800

    update

--- .ipynb_checkpoints/train_26b-checkpoint.sh
+++ .ipynb_checkpoints/train_26b-checkpoint.sh
@@ -12,8 +12,8 @@ export MASTER_PORT=34229
 export TF_CPP_MIN_LOG_LEVEL=3
 export LAUNCHER=pytorch
 
-OUTPUT_DIR="${ROOT}/finetuned/InternVL2-1B/internvl2_1b_qwen2_0_5b_dynamic_res_2nd_finetune_lora"
-MODEL_PATH="${ROOT}/pretrained/InternVL2-1B"
+OUTPUT_DIR="${ROOT}/finetuned/InternVL2-26B/internvl2_26b_internlm2_20b_dynamic_res_2nd_finetune_lora"
+MODEL_PATH="${ROOT}/pretrained/InternVL2-26B"
 META_PATH="${ROOT}/repos/internvl_workspace/finetune_custom.json"
 
 if [ ! -d "$OUTPUT_DIR" ]; then
@@ -28,7 +28,7 @@ torchrun \
   --master_port=${MASTER_PORT} \
   internvl/train/internvl_chat_finetune.py \
   --model_name_or_path ${MODEL_PATH} \
-  --conv_style "Hermes-2" \
+  --conv_style "internlm2-chat" \
   --output_dir ${OUTPUT_DIR} \
   --meta_path ${META_PATH} \
   --overwrite_output_dir True \
@@ -50,8 +50,8 @@ torchrun \
   --save_strategy "steps" \
   --save_steps 200 \
   --save_total_limit 1 \
-  --learning_rate 4e-5 \
-  --weight_decay 0.01 \
+  --learning_rate 2e-5 \
+  --weight_decay 0.05 \
   --warmup_ratio 0.03 \
   --lr_scheduler_type "cosine" \
   --logging_steps 1 \
@@ -61,8 +61,8 @@ torchrun \
   --group_by_length True \
   --dynamic_image_size True \
   --use_thumbnail True \
-  --ps_version "v2" \
-  --deepspeed "zero_stage1_config.json" \
+  --ps_version 'v2' \
+  --deepspeed "zero_stage3_config.json" \
   --report_to "tensorboard" \
   2>&1 | tee -a "${OUTPUT_DIR}/training_log.txt"
 
